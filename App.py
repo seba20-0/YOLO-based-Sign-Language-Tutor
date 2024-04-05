7697b4aa1777565a86f78e712eab50ff981ca584
Seba Al Mokdad <60008555+seba20-0@users.noreply.github.com>
2024-04-06 03:43:24 +0400

    Add routes

--- App.py
+++ App.py
@@ -1,17 +1,53 @@
-from flask import Flask, Response, session, render_template
+from flask import Flask, Response, session, render_template,request
 import cv2
 from YOLOWebcam import RunYOLOWebcam
 
 app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'seba'
 
+display_width = 546
+display_height = 275
+class_name=""
+last_classes=[]
+@app.route('/stop_detection', methods=['POST'])
+def stop_detection():
+    print(request.form)
+    global video_name
+    video_name = request.form.get('video_name').split('.')[0]
+    print(f'Video: {video_name}')
+    return video_name
+
 def generate_frames(path_x):
+    global class_name  # Declare class_name as global to modify it within this function
     yolo_output = RunYOLOWebcam(path_x)  
-    for detection_ in yolo_output:
-        ret, buffer = cv2.imencode('.jpg', detection_)
+    for detection_, classes in yolo_output:
+        if len(classes) > 0: 
+            global last_classes
+            last_classes=classes
+        if video_name != "" and class_name == video_name:
+                print(f'Video: {video_name}' , f'Class: {class_name}')
+                print("Detected class matches current video name. Stopping detection.")
+                break  # Exit the loop to stop the detection
+        #print(last_classes)
+        ref,buffer=cv2.imencode('.jpg',detection_)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n') 
+      # Reset class_name after iteration is done
+
+@app.route('/get_class')
+def get_class():
+    #global class_name
+    global last_classes
+    if len(last_classes) > 0: 
+        val = last_classes[0]
+        last_classes=[]
+        print(last_classes)
+        return val
+    return last_classes
+
+
+    
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
